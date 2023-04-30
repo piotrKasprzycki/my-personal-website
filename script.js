d26c2302d5386f988d5567b70d8d14a5be25f150
Piotr Kasprzycki <piotr.kasprzycki123@gmail.com>
2023-04-30 12:20:40 +0200

    Edit home section

---
+++ script.js
@@ -0,0 +1,22 @@
+const passionSpan = document.querySelector("#passion");
+
+const typewriter = new Typewriter(passionSpan, {
+  loop: true,
+  delay: 100,
+  deleteSpeed: 20,
+});
+
+typewriter
+  .pauseFor(300)
+  .typeString("I learn front-end development.")
+  .pauseFor(1000)
+  .deleteChars(30)
+  .pauseFor(300)
+  .typeString("I play drums.")
+  .pauseFor(1000)
+  .deleteChars(13)
+  .pauseFor(300)
+  .typeString("I make Neapolitan pizza.")
+  .pauseFor(1000)
+  .deleteChars(24)
+  .start();
